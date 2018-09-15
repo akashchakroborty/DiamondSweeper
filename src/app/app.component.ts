af364f5ddf7fd2fcf958dc7b8f0e7b21ac8c28d0
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2018-09-15 20:34:50 +0530

    adding initial table structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DiamondSweeper';
+export class AppComponent implements OnInit {
+  cellNumber: number = 0;
+  row_count: number = 8;
+  column_count: number = 8;
+  cellCordinates: object = {};
+  diamond_count: number = 8;
+  
+  ngOnInit(){
+   this.initializeGame();
+  }
+  
+  clickHandler = (e) =>{
+   console.log("Clicked");
+  }
+  
+  initializeGame() {
+    let table = document.createElement("table");
+    table.id = "diamond_container";
+    for (let row = 0; row < this.row_count; row++) {
+        let tr = document.createElement('tr');
+        for (let column = 0; column < this.column_count; column++) {
+            let td = document.createElement('td');
+            let div = document.createElement('div');
+            this.cellCordinates[this.cellNumber] = {
+                x_cord: row,
+                y_cord: column
+            };
+            div.className = "cell unknown";
+            div.id = `${this.cellNumber}`;
+            this.cellNumber++;
+            td.appendChild(div)
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.getElementById("diamondSweeper").appendChild(table);
+    document.getElementById("diamond_container").addEventListener("click", this.clickHandler);
+  }
 }
