840056efb5101ae4111d5db7ad79cdd68f8e5ccb
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2018-09-16 03:16:09 +0530

    adding randomGenerator for diamond & model Handler
    Expanded clickHandler functionality to include the score.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,44 @@ export class AppComponent implements OnInit {
   column_count: number = 8;
   cellCordinates: object = {};
   diamond_count: number = 8;
+  diamond_set: object = {};
+  win_count: number = 0;
+  display: string = 'none';
   
   ngOnInit(){
+   this.randomGenerators();
    this.initializeGame();
   }
   
+  openModal() {
+    this.display = "block";
+  }
+
+  onCloseHandled() {
+    this.display = 'none';
+  }
+  
+  randomGenerators() {
+    while (Object.keys(this.diamond_set).length < this.diamond_count) {
+      let randomnumber = Math.ceil(Math.random() * 63)
+      this.diamond_set[randomnumber] = randomnumber;
+    }
+  }
+  
   clickHandler = (e) =>{
-   console.log("Clicked");
+   if (e.target.nodeName == 'DIV') {
+      this.win_count++;
+      if (this.diamond_set[e.target.id]) {
+        e.target.className = "cell diamond disabled";
+        delete this.diamond_set[e.target.id];
+        if (Object.keys(this.diamond_set).length == 0) {
+          document.getElementById("openWinnerModalButton").click();
+        }
+        if (Object.keys(this.diamond_set).length == 1) {
+          alert("One more to go, Go and acheive it");
+        }
+      }
+    }
   }
   
   initializeGame() {
